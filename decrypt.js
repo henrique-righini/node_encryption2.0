f8ec29bfb0d9bb65f10da551d19b8e0931d9fbd6
Henrique Righini <henrique.righini@hotmail.com>
2021-02-05 21:23:40 +0000

    add key and log

--- decrypt.js
+++ decrypt.js
@@ -1,14 +1,16 @@
 var express = require("express");
-var myParser = require("body-parser");
+var bodyParser = require("body-parser");
 var app = express();
 var crypto = require("crypto");
 var fs = require('fs'); 
  
-   app.use(myParser.urlencoded({extended : true}));
+   app.use(bodyParser.json());
    app.post("/login", function(request, response) {
+       console.log(request.body.user);
+       console.log(request.body.pass);
     
-    privateKey = fs.readFileSync('priv.key').toString();
-    const decryptedData = crypto.privateDecrypt(privateKey, request.body)
+    //privateKey = fs.readFileSync('priv.key').toString();
+//const decryptedData = crypto.privateDecrypt(privateKey, request.body)
 });
 
 //Start the server and make it listen for connections on port 8080
