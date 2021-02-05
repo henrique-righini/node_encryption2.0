3858bdfd13e0dcea173fc0320c0d1afcd7d15169
Henrique Righini <henrique.righini@hotmail.com>
2021-02-05 21:00:29 +0000

    fix fs read key

--- decrypt.js
+++ decrypt.js
@@ -2,22 +2,13 @@ var express = require("express");
 var myParser = require("body-parser");
 var app = express();
 var crypto = require("crypto");
+var fs = require('fs'); 
  
    app.use(myParser.urlencoded({extended : true}));
    app.post("/login", function(request, response) {
-
-    const decryptedData = crypto.privateDecrypt(
-	{
-		key: privateKey,
-		// In order to decrypt the data, we need to specify the
-		// same hashing function and padding scheme that we used to
-		// encrypt the data in the previous step
-		padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-		oaepHash: "sha256",
-	},
-	request.body
-)
-       //console.log(request.body); //This prints the JSON document received (if it is a JSON document)
+    
+    privateKey = fs.readFileSync('priv.key').toString();
+    const decryptedData = crypto.privateDecrypt(privateKey, request.body)
 });
 
 //Start the server and make it listen for connections on port 8080
