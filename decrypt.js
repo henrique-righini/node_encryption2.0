5add31688e4d172030c4f69d1bac72befca9482e
Henrique Righini <henrique.righini@hotmail.com>
2021-02-05 20:21:40 +0000

    added crypto

--- decrypt.js
+++ decrypt.js
@@ -1,16 +1,24 @@
-//Import the necessary libraries/declare the necessary objects
-2. var express = require("express");
-3. var myParser = require("body-parser");
-4. var app = express();
-5. 
-6.   app.use(myParser.urlencoded({extended : true}));
-7.   app.post("/yourpath", function(request, response) {
-8.       console.log(request.body); //This prints the JSON document received (if it is a JSON document)
-9. });
-10. 
-11. //Start the server and make it listen for connections on port 8080
-12. 
-13. app.listen(8080);
+var express = require("express");
+var myParser = require("body-parser");
+var app = express();
+var crypto = require("crypto");
+ 
+   app.use(myParser.urlencoded({extended : true}));
+   app.post("/login", function(request, response) {
 
-sdfs
-sdfs
+    const decryptedData = crypto.privateDecrypt(
+	{
+		key: privateKey,
+		// In order to decrypt the data, we need to specify the
+		// same hashing function and padding scheme that we used to
+		// encrypt the data in the previous step
+		padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+		oaepHash: "sha256",
+	},
+	request.body
+)
+       //console.log(request.body); //This prints the JSON document received (if it is a JSON document)
+});
+
+//Start the server and make it listen for connections on port 8080
+app.listen(8080)
