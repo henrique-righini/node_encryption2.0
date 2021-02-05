422a270309d7e2ee8b52ec19ccd8aa9f223277ae
Henrique Righini <henrique.righini@hotmail.com>
2021-02-05 19:54:12 +0000

    Merge pull request #1 from henrique-righini/henrique-righini/gitpod-setup
    Fully automate dev setup with Gitpod

--- decrypt.js
+++ decrypt.js
@@ -1,1 +1,13 @@
-
+//Import the necessary libraries/declare the necessary objects
+2. var express = require("express");
+3. var myParser = require("body-parser");
+4. var app = express();
+5. 
+6.   app.use(myParser.urlencoded({extended : true}));
+7.   app.post("/yourpath", function(request, response) {
+8.       console.log(request.body); //This prints the JSON document received (if it is a JSON document)
+9. });
+10. 
+11. //Start the server and make it listen for connections on port 8080
+12. 
+13. app.listen(8080);
