e23f0f2888cba909448da3bb3f452878410091bc
Henrique Righini <henrique.righini@hotmail.com>
2021-02-08 17:34:46 +0000

    decryption ok!

--- decrypt.js
+++ decrypt.js
@@ -10,29 +10,42 @@ var fs = require('fs');
       // console.log(request.body.user);
       // console.log(request.body.pass);
       
-    privateKeyTxt = fs.readFileSync('priv.key').toString();
-    privateKey = crypto.createPrivateKey(privateKeyTxt);
-    //conferir objeto de chave privada
-    //console.log(privateKey);
+        privateKey = fs.readFileSync('priv.key').toString();
+        privateKey = crypto.createPrivateKey(privateKey);
+        //conferir objeto de chave privada
+        //console.log(privateKey);
    
-    let user = new Buffer.from(request.body.user, 'base64');
-    
-    console.log(user);
-    
-    const decryptedUser = crypto.privateDecrypt(
-        {
-		key: privateKey,
-		padding: crypto.constants.RSA_PKCS1_PADDING,
-		//oaepHash: "sha1",
-	    },
-        user
-    );
-        console.log(user.toString);
+        //console.log(request.body.user);
 
-    let pass = Buffer.from(request.body.pass, 'utf-8');
-   // console.log(pass.toString('utf-8'));
-    const decryptedPassword = crypto.privateDecrypt(privateKey, pass);
+        let user = new Buffer.from(request.body.user, 'base64');
+    
+        //console.log(user.toString());
+    
+        const decryptedUser = crypto.privateDecrypt(
+            {
+		    key: privateKey,
+		    padding: crypto.constants.RSA_PKCS1_PADDING,
+		    //oaepHash: "sha1",
+	        },
+            user
+        );
+        
 
+        let pass = Buffer.from(request.body.pass, 'base64');
+        // console.log(pass.toString('utf-8'));
+        const decryptedPassword = crypto.privateDecrypt(
+            {
+            key: privateKey,
+            padding: crypto.constants.RSA_PKCS1_PADDING, 
+            },
+            pass
+        );
+
+        user = decryptedUser.toString();
+        pass = decryptedPassword.toString();
+    
+        response.json({usernameDecrypted : user, passDecrypted : pass});
+    
     });
 
 
