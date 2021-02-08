f81d386e60289bd3aa08fd3aa712010891b634a8
Henrique Righini <henrique.righini@hotmail.com>
2021-02-08 13:31:29 +0000

    test

--- decrypt.js
+++ decrypt.js
@@ -3,11 +3,10 @@ var bodyParser = require("body-parser");
 var app = express();
 var crypto = require("crypto");
 var fs = require('fs'); 
- 
-<<<<<<< HEAD
 
-   app.use(bodyParser.json());
-   app.post("/login", function(request, response) {
+
+    app.use(bodyParser.json());
+    app.post("/login", function(request, response) {
        //console.log(request.body.user);
        //console.log(request.body.pass);
       
@@ -15,16 +14,7 @@ var fs = require('fs');
     privateKey = create.privateKey(privateKeyTxt);
     //conferir objeto de chave privada
     console.log(privateKey);
-    
-=======
-   app.use(bodyParser.json());
-   app.post("/login", function(request, response) {
-       //console.log(request.body.user);
-       //console.log(request.body.pass);
-      
-    privateKey = fs.readFileSync('priv.key').toString();
-
->>>>>>> fe1af763c607956cde166a20443221d7c5be8321
+   
     let user = new Buffer(request.body.user, 'base64');
     
     const decryptedUser = crypto.privateDecrypt(
@@ -37,9 +27,7 @@ var fs = require('fs');
     );
 
     console.log(user.toString('utf-8'));
-
-
-
+    
     let pass = Buffer.from(request.body.pass, 'utf-8');
     console.log(pass.toString('utf-8'));
     const decryptedPassword = crypto.privateDecrypt(privateKey, pass)
