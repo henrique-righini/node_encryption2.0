d05b712a69b7d14ddd0e0649e44d60809f477a57
Henrique Righini <henrique.righini@hotmail.com>
2021-02-12 15:13:29 +0000

    deactivated validation

--- decrypt.js
+++ decrypt.js
@@ -1,52 +1,48 @@
-const express = require("express");
-const {validate} = require("express-validator")
+const express = require('express');
+const cors = require('cors');
+const {body} = require('express-validator');
 const bodyParser = require("body-parser");
 const app = express();
-const crypto = require("crypto");
+const crypto = require('crypto');
 const fs = require('fs'); 
 
-
-    app.use(bodyParser.json());
-    app.post("/login", function(request, response) {
+    app.use(cors());
+    app.use(express.json());
+    app.post(
+        '/login',  
+        //body('login').escape(),
+        //descobrir como fazer blacklist pois .escape() quebra a criptografia
+        function(request, response) {
 
         console.log(request.body);
-        validate.('login').escape();
-        console.log(request.body);
-        
+
         privateKey = fs.readFileSync('priv.key').toString();
         //chave deve ser guardada em local seguro
 
-        let user = new Buffer.from(request.body.user, 'base64');
+        let login = new Buffer.from(request.body.login, 'base64');
         //input para descriptografar deve ser do tipo Buffer
-    
+        //console.log(login)
         const decryptedUser = crypto.privateDecrypt(
             {
 		    key: privateKey,
 		    padding: crypto.constants.RSA_PKCS1_PADDING,
 	        },
-            user
+            login
         );
         
-
-
-        let pass = Buffer.from(request.body.pass, 'base64');
-
-        const decryptedPassword = crypto.privateDecrypt(
-            {
-            key: privateKey,
-            padding: crypto.constants.RSA_PKCS1_PADDING, 
-            },
-            pass
-        );
-
-
-
-        user = decryptedUser.toString();
-        pass = decryptedPassword.toString();
-
-        response.json({usernameDecrypted : user, passDecrypted : pass});
+        var obj = JSON.parse(decryptedUser);
+         console.log(obj);
+         response.json(obj);
     
     });
 
+    app.post('/debugger', function(request, response) {
+
+        let debug = request.body.login;
+
+        response.json(JSON.stringify('esta aplicacao esta sendo debugada' , debug) );
+        
+        console.log('debugger');
+    });
 
     app.listen(8080);
